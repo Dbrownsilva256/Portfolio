511b80a574e25c1301fc0a333a3e1bb38bd5cd18
Dbrownsilva256 <35548819+Dbrownsilva256@users.noreply.github.com>
2018-02-07 19:22:24 -0700

    Update mapPage_01.js

--- mapPage_01.js
+++ mapPage_01.js
@@ -1,7 +1,7 @@
 function initialize() {
     var Diamond = new google.maps.LatLng(21.270734,-157.803682)
     var mapOptions = {
-        zoom: 15,
+        zoom: 11,
         center: Diamond,
         mapTypeId: google.maps.MapTypeId.HYBRID
     };
